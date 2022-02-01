3e925945add7293cc9a9ad8287995df1f9573295
Joseph Hooper <stayvaliant94@gmail.com>
2022-01-31 17:34:06 -0800

    Update styling to pair better on non Windows devices

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,19 +11,6 @@ const Footer = () => {
         <Link to="/" className={styles.homeLink}>
           <h1 className={styles.siteName}>CantPause</h1>
         </Link>
-
-        {/* <div className={styles.socialIcons}>
-          <a href="https://twitter.com/Th3HoopMan" target="_blank" rel="noopener noreferrer">
-            <img src={twitterLogo} alt="Twitter Logo"/>
-          </a>
-          <a href="https://open.spotify.com/show/3oNzUYeq8gfRal3MpklVOH?si=3781d5479cce41fe" target="_blank" rel="noopener noreferrer">
-            <img src={spotifyLogo} alt="Spotify Logo"/>
-          </a>
-          <a href="mailto:joseph@cantpause.com">
-            <img src={emailLogo} alt="Email Logo"/>
-          </a>
-        </div> */
-        }
         <SocialIcons/>
       </div>
     </div>
